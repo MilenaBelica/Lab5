04fa53abf73f946442076d7626e0b3763d636081
MilenaBelica <milena.belica@outlook.com>
2025-11-18 13:36:16 +0100

    zmiany

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -52,24 +52,66 @@ int main()
         }
     }
     */
-
+    /*
     //zadanie 3
     int tab2[10];
     int x, z;
-    int suma = 0;
+    float suma = 0;
+    float srednia = 0;
+    int num = 0;
+    int sec = time(NULL);
+    srand(sec);
 
     for (int i = 0; i < 10; i++) {
-        x = rand() % 10 + 1;
+        x = rand();
         tab2[i] = x;
         cout << tab2[i] << endl;
     }
     for (z = 0; z < 10; z++) {
         suma += tab2[z];
     }
-    cout << "suma elementow tablicy " << suma << endl;
-    if (tab2[z] < suma) {
-        cout << tab2[z];
+    srednia = suma / 10;
+    cout << "srednia arytmetyczna elementow tablicy " << srednia << endl;
+    for (z = 0; z < 10; z++) {
+        if (tab2[z] < srednia) {
+            num++;
+        }
+    }
+    cout << "Tyle jest liczb mniejszych od sredniej liczb z tablicy " << num << endl;
+
+    int min = tab2[0];
+    int max = tab2[0];
+
+    for (z = 1; z < 10; z++) {
+        if (tab2[z] < min) min= tab2[z];
+        if (tab2[z] > max) max = tab2[z];
+    }
+    cout << "Najmniejszy element w tablicy to " << min << endl;
+    cout << "Najwiekszy element w tablicy to " << max << endl;
+    */
+
+    //zadanie 4
+    int a; 
+    int b;
+
+    cout << "Podaj liczbe wierszy " << endl;
+    cin >> a;
+    cout << "Podaj liczbe kolumn " << endl;
+    cin >> b;
+     
+
+    int** table = new int* [a]; 
+    for (int i = 0; i < a; i++) {
+        table[i] = new int[b];  
     }
 
+    for (int i = 0; i < b; i++) {
+        for (int j = 0; j < b; j++) {
+            table[i][j];
+            cout << "Podaj wartosc dla komorki " << i << "  " << j;
+            cin >> table[i][j];
+        }
+        cout << endl;
+    }
 }
 
